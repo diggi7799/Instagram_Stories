3300b4300fb5ee407e7a656e6d8e09ada5571c65
diggi7799 <sdigvijay7799@gmail.com>
2023-10-09 23:35:29 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["Insta.html"],
+  theme: {
+    extend: {
+      colors: {
+        fuchisa: colors.fuchisa,
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
+
